61314bb8349227bfbab4f5084064edf1898247a4
cuijiayong <cuijiayong@sythealth.com>
2020-03-27 09:35:27 +0800

    first commit

---
+++ src/main/java/com/slefimpr/SourceCodeLearnApplication.java
@@ -0,0 +1,13 @@
+package com.slefimpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCodeLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceCodeLearnApplication.class, args);
+    }
+
+}
